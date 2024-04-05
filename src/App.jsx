69425e96c903b5b9147a4c97b2058bddcd2f6716
Sarina <sarina0618@gmail.com>
2024-04-05 20:39:31 +0900

    ✨ : ファビコンなど完成

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,10 @@ function App() {
       <div className='shine shine5'></div>
       <h1 className='title'>PCR値 計算</h1>
       <Form/>
-      <p>©ぴーしーあーるっち</p>
+      <div className='agreement'>
+        <a  className='link' href="https://forms.gle/Zritt53JFZLtKjrKA" target='blank'>お問い合わせ</a>
+        <p>©ぴーしーあーるっち</p>
+      </div>
     </div>
   );
 }
